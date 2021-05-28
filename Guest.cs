5d0785e5ae024d87dafc318e2a8fcf3f500de4d8
Sidney Crandall <sdncrandall5@gmail.com>
2021-05-28 16:05:47 -0500

    Dinner party complete

---
+++ Guest.cs
@@ -0,0 +1,14 @@
+using System;
+
+// Creating the key/Values for the guest list that is located in the Program.cs 
+namespace DinnerParty
+{
+    public class Guest
+    {
+        public string Name { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 
+using System.Collections.Generic;
+
+using System.Linq;
+
 namespace DinnerParty
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // All this is tied to the Guest.cs file
+            List<Guest> Guests = new List<Guest>()
+          {
+                new Guest {Name = "Marilyn Monroe", Occupation = "entertainer", Bio = "(1926 - 1962) American actress, singer, model"},
+                new Guest {Name = "Abraham Lincoln", Occupation = "politician", Bio = "(1809 - 1865) US President during American civil war"},
+                new Guest {Name = "Martin Luther King", Occupation = "activist", Bio = "(1929 - 1968)  American civil rights campaigner"},
+                new Guest {Name = "Rosa Parks", Occupation = "activist", Bio = "(1913 - 2005)  American civil rights activist"},
+                new Guest {Name = "Peter Sellers", Occupation = "entertainer", Bio = "(1925 - 1980) British actor and comedian"},
+                new Guest {Name = "Alan Turing", Occupation = "computer scientist", Bio = "(1912 - 1954) - British computing pioneer, Turing machine, algorithms, cryptology, computer architecture, saved the world"},
+                new Guest {Name = "Admiral Grace Hopper", Occupation = "computer scientist", Bio = "(1906–1992) - developed early compilers: FLOW-Matic, COBOL; worked on UNIVAC; gave speeches on computer history, where she gave out nano-seconds"},
+                new Guest {Name = "Indira Gandhi", Occupation = "politician", Bio = "(1917 - 1984) Prime Minister of India 1966 - 1977"}
+          };
+
+            // We need list to hold the data for the tables which guest will be divided betweem. hold the collection
+            List<Guest> Table1 = new List<Guest>();
+            List<Guest> Table2 = new List<Guest>();
+
+            foreach (Guest guest in Guests)
+            {
+                List<string> TableOccupation = Table1.Select(g => g.Occupation).ToList();
+                if (TableOccupation.Contains(guest.Occupation))
+                {
+                    Table2.Add(guest);
+                }
+                else
+                {
+                    Table1.Add(guest);
+                }
+            }
+
+            Console.WriteLine("Table One");
+            Console.WriteLine("--------------------------------------------");
+            foreach (Guest guest in Table1)
+            {
+                Console.WriteLine($"{guest.Name} {guest.Occupation} {guest.Bio}");
+            }
+            
+            Console.WriteLine("Table Two");
+            Console.WriteLine("--------------------------------------------");
+            foreach (Guest guest in Table2)
+            {
+                Console.WriteLine($"{guest.Name} {guest.Occupation} {guest.Bio}");
+            }
         }
     }
 }
